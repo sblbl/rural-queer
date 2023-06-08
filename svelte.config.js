b8dac3430d37b3cea5f2bfcda4019489bbc1c00a
Sblbl <marta.fioravanti.1996@gmail.com>
2023-06-08 23:24:31 +0200

    netlify sets

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 export default {
-    kit: {
-        adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
-        })
-    }
+  kit: {
+    adapter: adapter()
+  }
 };
